3e8ddec67790393edc3517525dc8735a1721075d
cristibp11 <32294283+cristibp11@users.noreply.github.com>
2019-01-20 15:35:25 +0100

    Primera aproximaci?n del software.

---
+++ src/src/gateway/gateway.ino
@@ -0,0 +1,20 @@
+#include <DallasTemperature.h>
+#include <DHT.h>
+#include <filter.h>
+#include <OneWire.h>
+#include <OpenGarden.h>
+#include <Ports.h>
+#include <RemoteReceiver.h>
+#include <RemoteSwitch.h>
+#include <RF12.h>
+#include <RTClib.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
