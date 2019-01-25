536e1107cba0ac9a3cfa46be3c5218e0d1b60d56
Cristina Bolaños Peño <cristinabope@gmail.com>
2019-01-25 11:03:39 +0100

    Eliminaci?n SubLC/hardware/ + Programa parcial Gateway

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# SISTEMA DE CONTROL DE RIEGO AUTOMATIZADO UTILIZANDO [OPEN GARDEN OUTDOOR KIT](https://www.cooking-hacks.com/open-garden-outdoor-1node-1gw) #
+# SISTEMA DE CONTROL DE RIEGO AUTOMATIZADO #
+## Utilizando [OpenGarden Outdoor Kit](https://www.cooking-hacks.com/open-garden-outdoor-1node-1gw) ##
 
 Trabajo de Fin de Grado para la Escuela Superior de Informática (Ciudad Real), Universidad de Castilla-La Mancha.
 
--- src/SubLC/src/gateway/gateway.ino
+++ src/SubLC/src/gateway/gateway.ino
@@ -1,3 +1,10 @@
+/*
+ * Author: Cristina Bolaños Peño
+ * SubLC_Gateway v1.0.0
+ * 
+ * Visit https://bitbucket.org/cristibp11/tfg.cristinabolanos/src/default/
+ */
+
 #include <DallasTemperature.h>
 #include <DHT.h>
 #include <filter.h>
@@ -9,12 +16,39 @@
 #include <RF12.h>
 #include <RTClib.h>
 
-void setup() {
-  // put your setup code here, to run once:
+DateTime now;
 
+void setup() {
+    Serial.begin(9600);
+    OpenGarden.initSensors();
+    OpenGarden.initRTC();
+    OpenGarden.setTime();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+    OpenGarden.sensorPowerON();
+    delay(500);
+
+    // Get time and print time of the day
+    now = OpenGarden.getTime();
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.print(',');
+    
+    // Read and print results as < luminosity(%),humidity(%RH),temperature(ºC),soilmoisture(values with out calibration) >
+    Serial.print(OpenGarden.readLuminosity());
+    Serial.print(',');
+    Serial.print(OpenGarden.readAirTemperature());
+    Serial.print(',');
+    Serial.print(OpenGarden.readAirHumidity());
+    Serial.print(',');
+    Serial.println(OpenGarden.readSoilMoisture());
+    
+    OpenGarden.sensorPowerOFF();  //Turns off the sensor power supply
+    delay(2000);   //Wait 2 seconds
+    //delay(600000);   //Wait 10 minutes
 }
